06ba99e86d722fb3e302d0c9dfc3c04394915aae
SantiFagliano <fagliano.santi@gmail.com>
2020-10-18 16:06:12 -0300

    agregamos header y footer como archivos aparte

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer>
+    <div class="bg-footer">
+        <p>Santiago Fagliano - 43103929 - <a href="mailto:santifagliano@live.com.ar">santifagliano@live.com.ar</a></p>
+    </div>
+</footer>
+<!-- Optional JavaScript; choose one of the two! -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+        crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <script src="js/functions.js"></script>
+
+
+    <title>SmiteDex</title>
+</head>
+
+<body>
+<header class="">
+    <nav class="d-flex flex-row b-3 p-2 justify-content-between">
+        <div class="centrar">
+            <a href="index.php"><img src="img/logo.png" alt="Logo"></a>
+        </div>
+        <div class="mt-4">
+            <form class="formulario d-flex flex-row justify-content-end" action="#">
+                <input type="text" name="usuario" id="usuario" placeholder="Usuario" class="form-control mr-1 mb-1">
+                <input type="password" name="contraseña" id="contraseña" placeholder="Contraseña"
+                       class="form-control mr-1 mb-2">
+
+                <button type="button" class="btn btn-outline-warning mb-1">Ingresar</button>
+            </form>
+        </div>
+    </nav>
+</header>
