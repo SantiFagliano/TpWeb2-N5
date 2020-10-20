b6bccb08196a38b8d1e4c74f6e004c03c8d0b2a6
SantiFagliano <fagliano.santi@gmail.com>
2020-10-20 19:39:16 -0300

    cambiamos las variables de la consulta

--- Consultas.php
+++ Consultas.php
@@ -8,36 +8,35 @@ function mostrarDiosesDeLaBaseDeDatos($conexion)
     while ($row = $resultado->fetch_assoc()) {
         $nombre = $row["nombre"];
         $panteon = $row["pantheon"];
-        $id = $row["id"];
+        $idDios = $row["id"];
         $rol = $row["rol"];
-
 
         echo '
             <div class="col-12 col-md-4 m-auto">
                     <div class="card mb-3 fondo">
-                        <img class="card-img-top" src="Recursos/img/dioses/' . $nombre . '.jpg" alt="Card image cap">
+                        <img class="card-img-top" src="Recursos/img/dioses/' . $nombre . '.jpg" alt="' . $nombre . '">
                         <div class="card-body border-top">
-                            <h2 class="card-title text-black">' . $id . ' | ' . $nombre . '</h2>
+                            <h2 class="card-title text-black">' . $idDios . ' | ' . $nombre . '</h2>
                             <hr>
-                            <p class="card-text font-x-large"><img src="Recursos/img/pantheon/' . $panteon . '.png" alt=""
+                            <p class="card-text font-x-large"><img src="Recursos/img/pantheon/' . $panteon . '.png" alt="' . $panteon . '"
                                                                    class="pantheon">' . $panteon . '
                             </p>
-                            <p class="card-text font-x-large"><img src="Recursos/img/tipo/' . $rol . '.png" alt=""
+                            <p class="card-text font-x-large"><img src="Recursos/img/tipo/' . $rol . '.png" alt="' . $rol . '"
                                                                    class="pantheon">' . $rol . '
                             </p>
                             <!-- Button trigger modal -->
                             <button type="button" class="btn btn-outline-primary" data-toggle="modal"
-                                    data-target="#staticBackdrop">
+                                    data-target="#staticBackdrop' . $idDios . '">
                                 Editar Dios
                             </button>
                             <!-- Button trigger modal -->
                             <button type="button" class="btn btn-outline-danger" data-toggle="modal"
-                                    data-target="#exampleModal">
+                                    data-target="#exampleModal' . $idDios . '">
                                 Eliminar Dios
                             </button>
 
                             <!-- Modal Eliminar dios-->
-                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                            <div class="modal fade" id="exampleModal' . $idDios . '" tabindex="-1" aria-labelledby="exampleModalLabel"
                                  aria-hidden="true">
                                 <div class="modal-dialog">
                                     <div class="modal-content">
@@ -55,23 +54,26 @@ function mostrarDiosesDeLaBaseDeDatos($conexion)
                                             <button type="button" class="btn btn-secondary" data-dismiss="modal">
                                                 Cancelar
                                             </button>
-                                            <button type="button" class="btn btn-primary">Confirmar</button>
+                                            <p>' . $idDios . '</p>
+                                            <a href="eliminarDios.php?id=' . $idDios . '" type="button" class="btn btn-primary">
+                                            Confirmar
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             <!-- Modal Editar dios-->
-                            <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false"
+                            <div class="modal fade" id="staticBackdrop' . $idDios . '" data-backdrop="static" data-keyboard="false"
                                  tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                                 <div class="modal-dialog">
                                     <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="staticBackdropLabel">Editar Dios</h5>
+                                        <div class="modal-header bg-editar">
+                                            <h5 class="modal-title text-white" id="staticBackdropLabel">Editar Dios</h5>
                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                 <span aria-hidden="true">&times;</span>
                                             </button>
                                         </div>
-                                        <div class="modal-body">
+                                        <div class="modal-body bg-editar">
                                             <form>
                                                 <div class="form-group mb-4">
                                                     <input type="text" class="nuevoDios text-white" name="nombre"
@@ -91,32 +93,32 @@ function mostrarDiosesDeLaBaseDeDatos($conexion)
                                                 <div class="input-group mb-4">
                                                     <select name="rol" class="custom-select nuevoDios" id="rol">
                                                         <option selected disabled>Eliga un rol:</option>
-                                                        <option value="support">Guardian</option>
-                                                        <option value="guerrero">Guerrero</option>
-                                                        <option value="cazador">Cazador</option>
-                                                        <option value="mago">Mago</option>
-                                                        <option value="asesino">Asesino</option>
+                                                        <option value="Guardian">Guardian</option>
+                                                        <option value="Guerrero">Guerrero</option>
+                                                        <option value="Cazador">Cazador</option>
+                                                        <option value="Mago">Mago</option>
+                                                        <option value="Asesino">Asesino</option>
                                                     </select>
                                                 </div>
 
                                                 <div class="input-group mb-4">
                                                     <select name="pantheon" class="custom-select nuevoDios" id="rol">
                                                         <option selected disabled>Eliga un Panteon:</option>
-                                                        <option value="arturiano">Arturiano</option>
-                                                        <option value="celta">Celta</option>
-                                                        <option value="chino">Chino</option>
-                                                        <option value="egipcio">Egipcio</option>
-                                                        <option value="eslavo">Eslavo</option>
-                                                        <option value="grandesAntiguos">Grandes Antiguos</option>
-                                                        <option value="griego">Griego</option>
-                                                        <option value="hindu">Hindu</option>
-                                                        <option value="japones">Japones</option>
-                                                        <option value="maya">Maya</option>
-                                                        <option value="nordico">Nordico</option>
-                                                        <option value="polinesio">Polinesio</option>
-                                                        <option value="romano">Romano</option>
-                                                        <option value="vudu">Vudu</option>
-                                                        <option value="yoruba">Yoruba</option>
+                                                        <option value="Arturiano">Arturiano</option>
+                                                        <option value="Celta">Celta</option>
+                                                        <option value="Chino">Chino</option>
+                                                        <option value="Egipcio">Egipcio</option>
+                                                        <option value="Eslavo">Eslavo</option>
+                                                        <option value="GrandesAntiguos">Grandes Antiguos</option>
+                                                        <option value="Griego">Griego</option>
+                                                        <option value="Hindu">Hindu</option>
+                                                        <option value="Japones">Japones</option>
+                                                        <option value="Maya">Maya</option>
+                                                        <option value="Nordico">Nordico</option>
+                                                        <option value="Polinesio">Polinesio</option>
+                                                        <option value="Romano">Romano</option>
+                                                        <option value="Vudu">Vudu</option>
+                                                        <option value="Yoruba">Yoruba</option>
                                                     </select>
                                                 </div>
                                                 <div class="form-group mb-4">
@@ -129,7 +131,7 @@ function mostrarDiosesDeLaBaseDeDatos($conexion)
                                                 <button type="submit" class="btn btn-nuevo">Subir nuevo Dios</button>
                                             </form>
                                         </div>
-                                        <div class="modal-footer">
+                                        <div class="modal-footer bg-editar">
                                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar
                                             </button>
                                             <button type="button" class="btn btn-primary">Guardar Cambios</button>
@@ -143,5 +145,10 @@ function mostrarDiosesDeLaBaseDeDatos($conexion)
 
         ';
     }
+    function borrarDios($conexion)
+    {
+        $sql = "insert into Dios (id, nombre, pantheon, rol, url_imagen, descripcion) VALUES () ";
 
+        $resultado2 = $conexion->query($sql);
+    }
 }
